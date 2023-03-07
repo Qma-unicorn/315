cd74ab6b2ba0916382a4e42ea3530b6de7ffbedf
qma <im.zhaslan2@gmail.com>
2023-03-07 17:24:23 +0600

    replace logic to two apps

--- logger.py
+++ logger.py
@@ -1,67 +1,76 @@
-from datetime import datetime
 import os
 import hashlib
+from datetime import datetime
 
 def main(folder):
-    paths = []
+    scansFolder = 'scans'
     newFile = []
+    now = str(datetime.now()) + '.txt'
     for root, dirs, files in os.walk(folder, topdown=False):
         for name in files:
-            paths.append(os.path.join(root, name))
-    # if '..' in folder:
-    #     folder = folder[3:]
-    # now = str(datetime.now()) + '.txt'
-    # if not os.path.isdir("scans"):
-    #     os.mkdir("scans")
-    # outfile = open('scans/' + folder + '_out' + now, 'a')
-    # outfile.close()
-    # outfile = open('scans/' + folder + '_out' + now, 'w')
+            try:
+                with open(os.path.join(root, name), 'r') as f:
+                    textFile = ''.join(f.read().splitlines())
+                    my_hash = hashlib.sha256(textFile.encode('utf-8')).hexdigest()
+                    newFile.append(str(os.path.join(root, name) + ': ' + my_hash + ' ' + '\n'))
+                    f.close()
+            except:
+                print(os.path.join(root, name))
 
-    for path in paths:
-        with open(path, 'r') as f:
-            textFile = ''.join(f.read().splitlines())
-            my_hash = hashlib.sha256(textFile.encode('utf-8')).hexdigest()
-            newFile.append(str(path + ': ' + my_hash + ' ' + '\n'))
-            f.close()
-        # outfile.write(path + ': ' + my_hash + ' ' + '\n')
-    print(newFile)
-    # outfile.close()
-    #
-    logs = []
-    for file in os.listdir('scans'):
-        if folder + '_out' in file:
-            logs.append('scans/' + os.path.join(file))
-    print(logs)
-    # if len(logs) >= 2:
-    #     files = sorted(logs)[-2:len(logs)]
-    #     newFile = open(files[1], 'r').readlines()
-    #     change = set([i.split(':')[0] for i in oldFile]) & set([i.split(':')[0] for i in newFile])
-    #     if oldFile == newFile:
-    #         print('success')
-    #     else:
-    #         print(oldFile)
-    #         print(newFile)
-    #         print('error')
-    #         noeq = {}
-    #         if not os.path.isdir("logs"):
-    #             os.mkdir("logs")
-    #         outfile = open('logs/' + folder + '_log' + now, 'a')
-    #         outfile.close()
-    #         outfile = open('logs/' + folder + '_log' + now, 'w')
-    #         outfile.write('удалено' + ':' + '\n')
-    #         for i in (set(oldFile) - set(newFile)):
-    #             if not i.split(':')[0] in change:
-    #                 outfile.write(i + '\n')
-    #                 oldFile.pop(oldFile.index(i))
-    #         outfile.write('создано' + ':' + '\n')
-    #         for i in set(newFile) - set(oldFile):
-    #             if not i.split(':')[0] in change:
-    #                 outfile.write(i + '\n')
-    #                 newFile.pop(newFile.index(i))
-    #         outfile.write('измененно' + ':' + '\n')
-    #         for i in range(len(newFile) - 1):
-    #             if newFile[i] != oldFile[i]:
-    #                 noeq[oldFile[i]] = newFile[i]
-    #                 outfile.write(oldFile[i] + ' = '+ newFile[i] + '\n')
-    #         outfile.close()
-main(input())
+    log = 0
+    for file in os.listdir(scansFolder):
+        if folder.split('/')[-1] + '_out' in file:
+            log = os.path.join(scansFolder, file)
+    oldFile = open(log, 'r').readlines()
+
+    change = set([i.split(':')[0] for i in oldFile]) & set([i.split(':')[0] for i in newFile])
+    if not os.path.isdir("logs"):
+        os.mkdir("logs")
+    outfile = open('logs/' + folder.split('/')[-1] + '_log' + now, 'a')
+    outfile.close()
+    if oldFile == newFile:
+        print('success')
+        outfile = open('logs/' + folder.split('/')[-1] + '_log' + now, 'w')
+        outfile.write('success')
+        outfile.close()
+    else:
+        print('error')
+        print(oldFile)
+        print(newFile)
+        print('\n')
+        noeq = {}
+        outfile = open('logs/' + folder.split('/')[-1] + '_log' + now, 'w')
+        outfile.write('удалено' + ':' + '\n')
+        for i in (set(oldFile) - set(newFile)):
+            if not i.split(':')[0] in change:
+                outfile.write(i + '\n')
+                oldFile.pop(oldFile.index(i))
+        outfile.write('создано' + ':' + '\n')
+        for i in set(newFile) - set(oldFile):
+            if not i.split(':')[0] in change:
+                outfile.write(i + '\n')
+                newFile.pop(newFile.index(i))
+        outfile.write('измененно' + ':' + '\n')
+        for i in range(len(newFile)):
+            if newFile[i] != oldFile[i]:
+                noeq[oldFile[i]] = newFile[i]
+                outfile.write(oldFile[i] + ' = '+ newFile[i] + '\n')
+        print(oldFile)
+        print(newFile)
+        outfile.close()
+
+start = datetime.now()
+file_name = "folder.txt"
+file_path = os.path.join(os.getcwd(), file_name)
+
+if os.path.isfile(file_path):
+    with open(file_name, 'r') as f:
+        path_to_target_file = f.readline().strip()
+    main(path_to_target_file)
+else:
+    print('целевая дериктория не указана')
+    print('создать файл ' + file_name)
+    print('укажите целую дерикторию')
+    outfile = open(file_name, 'a')
+    outfile.close()
+print(datetime.now() - start)
--- main.py
+++ main.py
@@ -4,63 +4,36 @@ import hashlib
 
 def main(folder):
     paths = []
-    for root, dirs, files in os.walk(folder, topdown=False):
-        for name in files:
-            paths.append(os.path.join(root, name))
-    if '..' in folder:
-        folder = folder[3:]
     now = str(datetime.now()) + '.txt'
     if not os.path.isdir("scans"):
         os.mkdir("scans")
-    outfile = open('scans/' + folder + '_out' + now, 'a')
+    outfile = open('scans/' + folder.split('/')[-1] + '_out' + now, 'a')
     outfile.close()
-    outfile = open('scans/' + folder + '_out' + now, 'w')
+    outfile = open('scans/' + folder.split('/')[-1] + '_out' + now, 'w')
 
-    for path in paths:
-        with open(path, 'r') as f:
-            textFile = ''.join(f.read().splitlines())
-            my_hash = hashlib.sha256(textFile.encode('utf-8')).hexdigest()
-        outfile.write(path + ': ' + my_hash + ' ' + '\n')
+    for root, dirs, files in os.walk(folder, topdown=False):
+        for name in files:
+            try:
+                with open(os.path.join(root, name), 'r') as f:
+                    textFile = ''.join(f.read().splitlines())
+                    my_hash = hashlib.sha256(textFile.encode('utf-8')).hexdigest()
+                outfile.write(os.path.join(root, name) + ': ' + my_hash + ' ' + '\n')
+            except:
+                print(os.path.join(root, name))
     outfile.close()
 
-    # logs = []
-    # for file in os.listdir('scans'):
-    #     if folder + '_out' in file:
-    #         logs.append('scans/' + os.path.join(file))
-    # if len(logs) >= 2:
-    #     files = sorted(logs)[-2:len(logs)]
-    #     oldFile = open(files[0], 'r').readlines()
-    #
-    #     newFile = open(files[1], 'r').readlines()
-    #     change = set([i.split(':')[0] for i in oldFile]) & set([i.split(':')[0] for i in newFile])
-    #     if oldFile == newFile:
-    #         print('success')
-    #     else:
-    #         print('error')
-    #         noeq = {}
-    #         new = []
-    #         delete = []
-    #         if not os.path.isdir("logs"):
-    #             os.mkdir("logs")
-    #         outfile = open('logs/' + folder + '_log' + now, 'a')
-    #         outfile.close()
-    #         outfile = open('logs/' + folder + '_log' + now, 'w')
-    #         outfile.write('удалено' + ':' + '\n')
-    #         for i in (set(oldFile) - set(newFile)):
-    #             if not i.split(':')[0] in change:
-    #                 outfile.write(i + '\n')
-    #                 oldFile.pop(oldFile.index(i))
-    #         outfile.write('создано' + ':' + '\n')
-    #         for i in set(newFile) - set(oldFile):
-    #             if not i.split(':')[0] in change:
-    #                 outfile.write(i + '\n')
-    #                 newFile.pop(newFile.index(i))
-    #         outfile.write('измененно' + ':' + '\n')
-    #         for i in range(len(newFile) - 1):
-    #             if newFile[i] != oldFile[i]:
-    #                 noeq[oldFile[i]] = newFile[i]
-    #                 outfile.write(oldFile[i] + ' = '+ newFile[i] + '\n')
-    #         outfile.close()
+start = datetime.now()
+file_name = "folder.txt"
+file_path = os.path.join(os.getcwd(), file_name)
 
-if __name__ == '__main__':
-    main(input('folder: '))
+if os.path.isfile(file_path):
+    with open(file_name, 'r') as f:
+        path_to_target_file = f.readline().strip()
+    main(path_to_target_file)
+else:
+    print('целевая дериктория не указана')
+    print('создать файл ' + file_name)
+    print('укажите целую дерикторию')
+    outfile = open(file_name, 'a')
+    outfile.close()
+print(datetime.now() - start)
